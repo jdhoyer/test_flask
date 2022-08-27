2645a68b15826a5e0a19cea0c6e83473122b551f
jd_hoyer <jd_hoyer@gmx.de>
2022-08-27 16:53:11 +0300

    yes

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app_flask = Flask(__name__)
-app_flask.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app_flask.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test2.db'
 db = SQLAlchemy(app_flask)
 
 
@@ -16,19 +16,53 @@ class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
     completed = db.Column(db.Integer, default=0)
-    date_create = db.Column(db.DateTime, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
         return 'Task %r' % self.id
 
-@app_flask.route('/')
+
+@app_flask.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue with your new task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app_flask.route('/delete/<int:id>')
+def delete(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'deletion failed'
+
+
+@app_flask.route('/update/<int:id>')
+def update(id):
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'deletion failed'
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    db.create_all()
     app_flask.run(debug=True)
-
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
